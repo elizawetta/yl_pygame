eee5f8e7218b76f324ab73c505a645c59e50857b
elizawetta <69101855+elizawetta@users.noreply.github.com>
2022-01-09 20:48:12 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ pygame.init()
 pygame.display.set_caption('')
 size = width, height = 1400, 800
 screen = pygame.display.set_mode(size)
+screen_speed = 200
+chel_speed = 250
 
 
 def add_cubes(file_name):
@@ -19,18 +21,14 @@ def add_cubes(file_name):
     for i, row in enumerate(file):
         for col, el in enumerate(row):
             if el in 'bgyrs':
-                Cube(cubes, pos=(col * 50 + 300, i * 50), img=el)
+                Cube(cubes, pos=(col * 50 + 300, i * 50 + 150), img=el)
+                Line(lines, pos=(col * 50 + 300, i * 50 + 150))
+
                 end = max(end, col)
 
 
-def draw_play_screen():
-    screen.fill((204, 255, 255))
-    pygame.draw.rect(screen, 'black', (0, 800 - 150, 1400, 150))
-    cubes.draw(screen)
-    chel.draw(screen)
-
-
 def check_event(event):
+    # TODO: проверка на то, что мышка не находится на кнопках
     if event.type == pygame.MOUSEBUTTONDOWN:
         return True
     # if event.type == pygame.KEYDOWN:
@@ -43,35 +41,70 @@ def check_jump(a):
     global in_jump, up_flag, up
     if in_jump and up_flag:
         chel.update(chel_speed * seconds)
+        line_chel.update(chel_speed * seconds)
         up -= chel_speed * seconds
         if up <= 0:
             up_flag = False
-            for i in chel:
-                i.rect.y = 800 - 150 - 60 - a
+            chel.rect.y = level- a
+            line_chel.rect.y = level - a
     if in_jump and not up_flag:
         chel.update(-chel_speed * seconds)
+        line_chel.update(-chel_speed * seconds)
         up += chel_speed * seconds
         if up >= a:
             in_jump = False
-            for i in chel:
-                i.rect.y = 800 - 150 - 60
+            up_flag = False
+            chel.rect.y =  level
+            line_chel.rect.y = level + chel.height
+
+
+def check_cross():
+    global in_jump
+    global chel
+    global level
+    global run_chel
+    for i in lines:
+        if pygame.sprite.collide_mask(chel, i):
+            if abs(chel.height + chel.rect.y - i.rect.y) < 4:
+                level = chel.rect.y
+                in_jump, up_flag = False, False
+            else:
+                run_chel = False
+                # TODO: при столкновении на уровень опускаться
+
+
+def draw_play_screen():
+    screen.fill((204, 255, 255))
+    pygame.draw.rect(screen, 'black', (0, height - 150, width, 150))
+    sett_img = load_image('settings_img.png', 'images')
+    pause_img = load_image('pause_img.png', 'images')
+    repeat_img = load_image('repeat_img.png', 'images')
+
+    screen.blit(sett_img, (10, 10))
+    screen.blit(pause_img, (10, 60))
+    screen.blit(repeat_img, (10, 110))
+
+    lines.draw(screen)
+    cubes.draw(screen)
+    chel.draw(screen)
+    line_chel.draw(screen)
 
 
 cubes = pygame.sprite.Group()
+lines = pygame.sprite.Group()
 add_cubes('lvl_1.txt')
-chel = pygame.sprite.Group()
-Chel(chel)
+chel = Chel()
+line_chel = ChelLine()
+
 draw_play_screen()
 
 pygame.display.flip()
 elapsed = 0
-# clock = pygame.time.Clock()
 end = end * 50 + 100
 running = True
 run_chel = False
 in_jump = False
-screen_speed = 200
-chel_speed = 205
+level = chel.rect.y
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -80,18 +113,22 @@ while running:
             run_chel = not run_chel
             clock = pygame.time.Clock()
         if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE and run_chel and not in_jump:
-            up, up_flag = 65, True
+            up, up_flag = 62, True
             in_jump = True
     if run_chel:
         seconds = elapsed / 1000.0
         end -= screen_speed * seconds
         elapsed = clock.tick(30)
-        draw_play_screen()
 
         if end >= 0:
             cubes.update(screen_speed * seconds)
+            lines.update(screen_speed * seconds)
 
         if in_jump:
-            check_jump(65)
-
+            check_jump(62)
+        check_cross()
+    draw_play_screen()
     pygame.display.flip()
+    # print(chel.rect)
+
+
